b5ff9c137a4e0f078a2b8530dc09f20c028df16a
Muhammad Awais Abdullah <143254491+Awadul@users.noreply.github.com>
2025-08-31 20:03:06 +0500

    cc

--- src/data/targetAudienceData.js
+++ src/data/targetAudienceData.js
@@ -6,25 +6,25 @@
 const targetAudienceImages = {
     // Content Creators audience image
     contentCreators: {
-        path: '/assets/audience-img-1.jpg',
+        path: './src/assets/audience-img-1.jpg',
         alt: 'Content Creators - Social media influencers creating engaging content'
     },
 
     // Small Business audience image
     smallBusiness: {
-        path: '/assets/audience-img-2.jpg',
+        path: './src/assets/audience-img-2.jpg',
         alt: 'Small Business - Local business owners using social media marketing'
     },
 
     // Freelancers audience image
     freelancers: {
-        path: '/assets/audience-img-3.jpg',
+        path: './src/assets/audience-img-3.jpg',
         alt: 'Freelancers - Independent professionals growing their client base'
     },
 
     // Marketing Agencies audience image
     marketingAgencies: {
-        path: '/assets/audience-img-4.jpg',
+        path: './src/assets/audience-img-4.jpg',
         alt: 'Marketing Agencies - Professional teams delivering social media services'
     }
 };
